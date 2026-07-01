25697a05278818625d4940603e56f93161d1d8bb
Emilio Zuñiga <emizc2004@gmail.com>
2026-07-01 10:47:11 -0600

    ADD: Competencia 2 | Ejercicio 3

---
+++ Competencia2/Ejercicio3/Text.py
@@ -0,0 +1,16 @@
+from antlr4 import *
+from ExprLexer import ExprLexer
+
+entrada = "edad = 18"
+
+#lexer = ExprLexer(InputStream(input("?")))
+lexer = ExprLexer(InputStream(entrada))
+
+tokens = CommonTokenStream(lexer)
+tokens.fill()
+print(tokens)
+
+
+
+for token in tokens.tokens:
+    print("Texto: ", token.text, " Tipo: ", lexer.symbolicNames[token.type])
